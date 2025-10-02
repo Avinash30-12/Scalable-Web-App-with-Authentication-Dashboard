f3aa54edcbd00359eb3b72bf02bf5470a49204d7
Avinash Jha <avinashjha3012@gmail.com>
2025-10-02 21:58:19 +0530

    Update login.js

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -48,26 +48,28 @@ export default function Login() {
         <form onSubmit={handleSubmit} className="space-y-5">
           <div className="relative">
           <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              className="w-full px-4 py-3 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-              value={form.email}
-              onChange={handleChange}
-              required
-            />
+  type="email"
+  name="email"
+  placeholder="Email"
+  className="w-full px-4 py-3 border rounded-xl text-black placeholder-black 
+             focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+  value={form.email}
+  onChange={handleChange}
+  required
+/>
           </div>
 
           <div className="relative">
             <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              className="w-full px-4 py-3 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-              value={form.password}
-              onChange={handleChange}
-              required
-            />
+  type="password"
+  name="password"
+  placeholder="Password"
+  className="w-full px-4 py-3 border rounded-xl text-black placeholder-black
+             focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+  value={form.password}
+  onChange={handleChange}
+  required
+/>
           </div>
 
           <button
